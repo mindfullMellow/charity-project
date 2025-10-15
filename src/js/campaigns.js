1670170dca2437be0d033a8cfb81053e1f602cdf
Daniel Samuel <samueldaniel4198@gmail.com>
2025-10-15 07:49:06 +0100

    worked  on the campaign and contact html

--- src/js/campaigns.js
+++ src/js/campaigns.js
@@ -28,60 +28,60 @@ fetch('../../index.html').then(res => res.text())
   })
 
 
-///////////////////////////////////////
-//switching tab
-const tabContainer = document.getElementById('tab-container');
-const tabBtns = document.querySelectorAll('.tab-btn');
-const tabContent = document.querySelectorAll('.tab-content')
-function addOpacityToBtns() {
-  tabBtns.forEach(cur => {
-    if (!cur.classList.contains('active-tab'))
-      cur.classList.add('opacity-80')
+function switchTabInit() {
+  ///////////////////////////////////////
+  //switching tab
+  const tabContainer = document.getElementById('tab-container');
+  const tabBtns = document.querySelectorAll('.tab-btn');
+  const tabContent = document.querySelectorAll('.tab-content')
+  function addOpacityToBtns() {
+    tabBtns.forEach(cur => {
+      if (!cur.classList.contains('active-tab'))
+        cur.classList.add('opacity-80')
 
-  })
-}
-
-
-//First add opacity to the non active class
-addOpacityToBtns()
-
-
-
-tabContainer.addEventListener('click', (e) => {
-
-  //I couldnt figure out using opacity because the  elemnts still shows so i added a slight delay 
-  setTimeout(() => {
-    //Making sure we get  a result only when the el with the "tab-btn" is clicked
-    if (!e.target.classList.contains('tab-btn')) return;
-
-    //Find the target
-    const target = e.target
-
-    //find the elment that its id is the same as the clicked/active btn data-tab
-    const targetContent = target.closest('.parent-container').querySelector(`#${target.dataset.tab}`)
-
-
-    //Check if target has the active class
-    if (target.classList.contains('active-tab')) return;
-
-
-    //remove the opacity and active class from the btns
-    tabBtns.forEach(cur => cur.classList.remove('active-tab', 'opacity-80'))
-
-    //add active class to the clicked btn
-    target.classList.add('active-tab')
-
-    //add opacity to the non-active tab 
-    addOpacityToBtns()
-
-    //remove the hidden class from all the tab content 
-    tabContent.forEach(cur => cur.classList.remove('hidden'))
-
-    //if the tab content id doesnt match the clickedbtn/target dataset add the hidden class
-    tabContent.forEach(cur => {
-      if (cur.id !== targetContent.id)
-        cur.classList.add('hidden')
     })
-  }, 50)
+  }
 
-});
+
+  //First add opacity to the non active class
+  addOpacityToBtns()
+  tabContainer.addEventListener('click', (e) => {
+    //I couldnt figure out using opacity because the  elemnts still shows so i added a slight delay 
+    setTimeout(() => {
+      //Making sure we get  a result only when the el with the "tab-btn" is clicked
+      if (!e.target.classList.contains('tab-btn')) return;
+
+      //Find the target
+      const target = e.target
+
+      //find the elment that its id is the same as the clicked/active btn data-tab
+      const targetContent = target.closest('.parent-container').querySelector(`#${target.dataset.tab}`)
+
+
+      //Check if target has the active class
+      if (target.classList.contains('active-tab')) return;
+
+
+      //remove the opacity and active class from the btns
+      tabBtns.forEach(cur => cur.classList.remove('active-tab', 'opacity-80'))
+
+      //add active class to the clicked btn
+      target.classList.add('active-tab')
+
+      //add opacity to the non-active tab 
+      addOpacityToBtns()
+
+      //remove the hidden class from all the tab content 
+      tabContent.forEach(cur => cur.classList.remove('hidden'))
+
+      //if the tab content id doesnt match the clickedbtn/target dataset add the hidden class
+      tabContent.forEach(cur => {
+        if (cur.id !== targetContent.id)
+          cur.classList.add('hidden')
+      })
+    }, 50)
+  });
+
+
+}
+switchTabInit()
