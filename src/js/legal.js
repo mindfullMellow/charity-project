b1fe1edfe4c7eb51ca8150e8ce80671d6a2967a9
Daniel Samuel <samueldaniel4198@gmail.com>
2025-10-26 10:02:34 +0100

    started buiding the privacy and ToS docs

---
+++ src/js/legal.js
@@ -0,0 +1,39 @@
+'use strict'
+import * as utils from './utilis'
+
+
+fetch('../../index.html').then(res => res.text())
+  .then(data => {
+    //convert the fecthed html string into a DOM 
+    const parser = new DOMParser();
+    const doc = parser.parseFromString(data, 'text/html');
+
+
+    // get the <header> element from that HTML
+    const header = doc.querySelector('header');
+    const modalComponents = doc.querySelector('.modal-components')
+    const volunteerModal = doc.querySelector('.volunteer-modal-component')
+    const footer = doc.querySelector('footer')
+
+    // add it to the current page
+    if (header) document.body.prepend(header); // or appendChild
+    if (modalComponents) document.querySelector('main').after(modalComponents)
+    if (volunteerModal) document.querySelector('.modal-components').after(volunteerModal)
+    if (footer) document.querySelector('.modal-components').after(footer)
+  }).then(() => {
+    //  EVERY ELEMENT THAT DEPENDS ON THIS HTML BEING FECTHED SHOULD BE HERE 
+
+    //MOBILE NAV LOGIC
+    utils.mobileNav()
+
+    utils.ModalInit()
+    utils.volunteerModalInit()
+
+    //Adding sticky nav
+    // utils.addStickyNav(document.querySelector('.news-hero-section'))
+
+
+    // get full year from utils
+    document.getElementById('year').textContent = utils.getFullYear()
+
+  })
