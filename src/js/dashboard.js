3ded4eb3b5f503ce0e2146fc84a8e2296de59977
Daniel Samuel <samueldaniel4198@gmail.com>
2025-11-01 01:44:17 +0100

    created the dashboard for membership

---
+++ src/js/dashboard.js
@@ -0,0 +1,21 @@
+"use strict";
+
+
+const closeSideMenuBTN = document.querySelector('.close-sideMenu')
+const AllMenuNavEL = document.querySelectorAll('.menu-nav')
+const sidebar = document.querySelector('.w-\\[30\\%\\]')
+
+console.log(AllMenuNavEL);
+
+// closeSideMenuBTN.addEventListener("click", () => {
+//   sidebar.classList.remove('w-[30%')
+//   sidebar.classList.add('w-[5%]')
+
+
+//   AllMenuNavEL.forEach(cur => {
+//     cur.querySelectorAll('p').forEach(cur => {
+//       console.log(cur);
+//       cur.style.display = 'none'
+//     })
+//   })
+// })
